c53d3e90ef6427d12839429d16e62c804d88366b
Michael Strommer <69633126+mstrommer@users.noreply.github.com>
2021-11-09 11:39:11 +0100

    Initial commit

---
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        // test your method implementations here
+        // make method calls
+        // print their results
+        // etc.
+    }
+}
